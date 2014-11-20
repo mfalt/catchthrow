52f83da3bca039efa29e450f20978a8ba2479859
Lucas Jimbergsson <lucas@jimbergsson.se>
2014-11-20 18:30:02 +0100

    Some code in TrajectoryRef.java, now calling jmatio.

--- code/src/TrajectoryRef.java
+++ code/src/TrajectoryRef.java
@@ -1,6 +1,19 @@
+import java.io.IOException;
+
+import com.jmatio.io.MatFileReader;
+
 
 public class TrajectoryRef extends ReferenceGenerator {
 
+	public TrajectoryRef(String file) {
+		try {
+			MatFileReader fileReader = new MatFileReader(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public double getRef() {
 		return 0;
