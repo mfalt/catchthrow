b7f4e735b8b94afac4df15588ff068041abb8e19
Lucas Jimbergsson <lucas@jimbergsson.se>
2014-11-27 10:04:37 +0100

    no comment

--- code/src/ReferenceGenerator.java
+++ code/src/ReferenceGenerator.java
@@ -1,11 +1,7 @@
 public abstract class ReferenceGenerator {
 	
+	public final int POS=0, VEL=1, ANGLE=3, ANGLEVEL=4; 
 	private long t0 = -1;
-	
-	/**
-	 * References in the order:
-	 * (posRef, velRef, angleRef, angleVelRef) 
-	 */
 	protected double ref[] = new double[4];
 	
 	public abstract double[] getRef();
