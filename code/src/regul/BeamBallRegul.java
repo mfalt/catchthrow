cb050f602e3a8365eb06dd87e553a0b9554bbc51
Lucas Jimbergsson <lucas@jimbergsson.se>
2014-12-03 06:46:03 +0100

    (hopefully) fixed ball control PID parameters for SI units

--- code/src/regul/BeamBallRegul.java
+++ code/src/regul/BeamBallRegul.java
@@ -18,7 +18,7 @@ public class BeamBallRegul extends Regul {
 		inner = in;
 		p = new PIDParameters();
 		//inner = new BeamRegul();
-		p.K = -0.05 / RegulThread.metersPerVolt;
+		p.K = -0.05 * RegulThread.radiansPerVolt / RegulThread.metersPerVolt;
 		p.Ti = 20.0;
 		p.Tr = 10.0;
 		p.Td = 3.0;
