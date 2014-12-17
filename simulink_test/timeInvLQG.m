0b1af3e07283f57b81e5794f4f51917157398d16
Lucas Jimbergsson <lucas@jimbergsson.se>
2014-12-17 17:56:39 +0100

    Almost done, but riccati problem

--- simulink_test/timeInvLQG.m
+++ simulink_test/timeInvLQG.m
@@ -3,6 +3,7 @@ est_JB;
 m = 0.1; % Do not know best mass to linearize around
 x1 = 0; % Ball position
 x0 = [x1; 0; 0; 0; m; 0]; % Last state linearized around u according to report..?
+n = length(x0);
 h = 0.01;
 
 A = [0 1 0 0 0 0;
@@ -23,8 +24,18 @@ Phi = discsys.a;
 Gamma = discsys.b;
 
 %% Solve riccati equations
-[??] = dare(???);
-L = ??;
+% [~,~,L] = dare(Phi,Gamma,Q,R);
+
+Q = diag([1111,10000,816,13131,0,25]);
+R = 1;
+N = zeros(n,1);
+eig([Q N;N' R])
+L = lqr(discsys, Q, R, N) % Minimizes J = Sum {x'Qx + u'Ru + 2*x'Nu}
+
+Qn = eye(6);
+Rn = eye(2);
+Nn = zeros(n,2);
+[~,K,~] = kalman(discsys, Qn, Rn, Nn); % Qn state noise var, Rn measurement noise var, Nn cross terms.
 
 %% Store system
 save timeInvLQG.mat Phi Gamma C D L K x0;
