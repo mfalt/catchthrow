9bd779215f2fff67d228684526649dfb0273ac12
Lucas Jimbergsson <lucas@jimbergsson.se>
2014-12-17 23:37:36 +0100

    Riccati works!

--- simulink_test/timeInvLQG.m
+++ simulink_test/timeInvLQG.m
@@ -12,10 +12,22 @@ A = [0 1 0 0 0 0;
     m*g/JB 0 kB/JB 0 g*x1/JB 0;
     0 0 0 0 0 0;
     1 0 0 0 0 0];
-B = [0; 0; 0; ku/JB; 0; 0];
+
+% B = [0; 0; 0; ku/JB; 0; 0];
+B = [0 0; 0 0; 0 0; ku/JB 0; 0 1; 0 0]; %u2 controls mass
+
+% C = [1 0 0 0 0 0;
+%     0 0 1 0 0 0];
+% C = [1 0 0 0 0 0;
+%     0 0 1 0 0 0;
+%     0 0 0 0 0 1]; % y3 observes integral pos
 C = [1 0 0 0 0 0;
-    0 0 1 0 0 0];
-D = [0; 0];
+    0 0 1 0 0 0;
+    0 0 0 0 1 0; % y3 observes mass
+    0 0 0 0 0 1]; % y4 observes integral pos
+
+D = zeros(size(C,1), size(B,2));
+
 
 %% Discretize system
 contsys = ss(A, B, C, D);
@@ -24,18 +36,30 @@ Phi = discsys.a;
 Gamma = discsys.b;
 
 %% Solve riccati equations
+Q = diag([1111,10000,816,13131,0,25]);
+% R = 1;
+% N = zeros(n,1);
+R = diag([1 1e10]);
+N = zeros(n,length(R));
 % [~,~,L] = dare(Phi,Gamma,Q,R);
 
-Q = diag([1111,10000,816,13131,0,25]);
-R = 1;
-N = zeros(n,1);
-eig([Q N;N' R])
-L = lqr(discsys, Q, R, N) % Minimizes J = Sum {x'Qx + u'Ru + 2*x'Nu}
+L = lqr(discsys, Q, R, N); % Minimizes J = Sum {x'Qx + u'Ru + 2*x'Nu}
 
-Qn = eye(6);
-Rn = eye(2);
-Nn = zeros(n,2);
-[~,K,~] = kalman(discsys, Qn, Rn, Nn); % Qn state noise var, Rn measurement noise var, Nn cross terms.
+Qn =diag([0 1 0 1 1 0]);
+% Qn = eye(n);
+Rn = diag([1 1 1e10 1e10]);
+Nn = zeros(n,length(Rn));
+kalman_artif_sys = ss(discsys.a', discsys.c', zeros(1,n), zeros(1,size(discsys.c,1)), h);
+K = lqr(kalman_artif_sys, Qn, Rn, Nn)';
+% [~,K,~] = kalman(discsys, Qn, Rn, Nn); % Qn state noise var, Rn measurement noise var, Nn cross terms.
+
+%% Remove u2
+L = L(1,:)
+B = B(:,1);
+D = D(1:2,1);
+C = C(1:2,:);
+K = K(:,1:2)
+
 
 %% Store system
 save timeInvLQG.mat Phi Gamma C D L K x0;
