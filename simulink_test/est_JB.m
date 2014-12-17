92e9b098268915a155f986bc8d7f8840496abf66
Lucas Jimbergsson <lucas@jimbergsson.se>
2014-12-17 11:37:07 +0100

    Almost done estimating J_B

--- simulink_test/est_JB.m
+++ simulink_test/est_JB.m
@@ -1,9 +1,11 @@
 %%
 load JB_data;
 tphi = angle.Time;
-phi = angle.Data;
+phisc = angle.Data;
 tu = u.Time;
 uval = u.Data;
+
+n = 10; % Level of smoothing
 
 % figure;
 % plot(phi);
@@ -11,24 +13,35 @@ uval = u.Data;
 % figure;
 % plot(uval);
 
-phiacc = filter([-1 2 -1], 1, phi) ./ tphi.^2;
-% phiacc = diff(phi) ./ tphi(2:end);
-
+hphi = diff(tphi);
+hphi = hphi(1:end-1);
+h2phi = diff(diff(tphi));
 tphi = tphi(2:end-1);
-phi = phi(2:end-1);
+phisc = phisc(2:end-1);
 tu = tu(2:end-1);
 uval = uval(2:end-1);
-phiacc = phiacc(2:end-1);
+
+filt = -smooth_diff(n);
+phiscvel = filter(filt, 1, phisc) ./ hphi;
+phiscacc = filter(conv(filt, filt), 1, phisc) ./ hphi.^2;
+
+tphi = tphi(n+1:end-n);
+phisc = phisc(n+1:end-n);
+tu = tu(n+1:end-n);
+uval = uval(n+1:end-n);
+phiscacc = phiscacc(n+1:end-n);
+phiscvel = phiscvel(n+1:end-n);
 
 %%
-acc_idx = abs(phiacc) > 
-tphi = tphi(2:end-1);
-phi = phi(2:end-1);
-tu = tu(2:end-1);
-uval = uval(2:end-1);
-phiacc = phiacc(2:end-1);
+acc_idx = abs(phiscacc) > 10;
+tphi = tphi(acc_idx);
+phisc = phisc(acc_idx);
+tu = tu(acc_idx);
+uval = uval(acc_idx);
+phiscacc = phiscacc(acc_idx);
 
 % Do not forget scaling on phi
+JB_samples = 
 
 
 
