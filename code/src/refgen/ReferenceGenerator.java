c61070fd4e2f2978e7817e84f7eef18df454a935
Mattias Gustavsson Falt <atp10mfa@heron-01.control.lth.se>
2014-12-19 15:59:45 +0100

    RefGen methods

--- code/src/refgen/ReferenceGenerator.java
+++ code/src/refgen/ReferenceGenerator.java
@@ -4,13 +4,18 @@ public abstract class ReferenceGenerator {
 
 	public static final int POS=0, VEL=1, ANGLE=2, ANGLEVEL=3; 
 	protected double ref[] = new double[4];
+	private long t0 = -1;
 	
 	public abstract double[] getRef();
 
+	public void resetTime() {
+		t0 = System.currentTimeMillis();
+	}
+
+	protected double getTimeSeconds() {
+		return (System.currentTimeMillis() - t0) * 0.001;
+	}
 }
-
-
-
 
 
 
